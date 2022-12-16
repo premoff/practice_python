4804b371f396ed12ecc5045d8387b1371b35548d
premoff <111107248+premoff@users.noreply.github.com>
2022-12-16 12:32:01 +0530

    Sum square difference

--- SumSqaureDiff.py
+++ SumSqaureDiff.py
@@ -4,12 +4,12 @@
 # Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum. 
 
 n = int(input('enter a number: '))
-#sum of the suares 1-n natural numbers
+#sum of the suares 1 to n natural numbers
 sum_of_squares = 0
 for i in range(1,n+1):
     sum_of_squares += i**2
 
-#sum of the squares of 1-n natural numbers
+#sum of the squares of 1 to n natural numbers
 square_of_sum = 0
 for j in range(1,n+1):
     square_of_sum += j
